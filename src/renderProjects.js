80982973c8c2cb75784423a2d7a6eab53b83a05b
Scott Schwab <schwabpiano@gmail.com>
2022-03-09 17:10:08 -0600

    Remove old entries before adding entries from array

--- src/renderProjects.js
+++ src/renderProjects.js
@@ -1,15 +1,39 @@
-
 import { defaultProject } from "./projects";
 
 const renderProject = () => {
+    let content = document.getElementById("content");
+
+    const entries = document.getElementsByClassName("entry");
+    while (entries.length > 0) {
+        entries[0].parentNode.removeChild(entries[0]);
+    }
+
+
+    let entryHolder = document.createElement("div");
+    content.appendChild(entryHolder);
+    entryHolder.classList.add("entryHolder");
+
     defaultProject.forEach(item => {
-        let content = document.getElementById("content");
         let entry = document.createElement("div");
-        content.appendChild(entry);
+        entry.classList.add("entry");
+        entryHolder.appendChild(entry);
+
 
         let title = document.createElement("li");
         title.textContent = item.title;
         entry.appendChild(title);
+
+        let description = document.createElement("li");
+        description.textContent = item.description;
+        entry.appendChild(description);
+
+        let dueDate = document.createElement("li");
+        dueDate.textContent = item.dueDate;
+        entry.appendChild(dueDate);
+
+        let priority = document.createElement("li");
+        priority.textContent = item.priority;
+        entry.appendChild(priority);
 
     });
 }
